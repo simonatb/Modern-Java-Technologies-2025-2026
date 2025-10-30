b7edf3b91a5df236041cc1b12edd2e3aad72b3fb
simonatb <149167261+simonatb@users.noreply.github.com>
2025-10-30 15:58:17 +0200

    Delete Labs/The Bachelor directory

--- Labs/The Bachelor/src/bg/sofia/uni/fmi/mjt/show/elimination/EliminationRule.java
+++
@@ -1,9 +0,0 @@
-package bg.sofia.uni.fmi.mjt.show.elimination;
-
-import bg.sofia.uni.fmi.mjt.show.ergenka.Ergenka;
-
-public interface EliminationRule {
-
-    Ergenka[] eliminateErgenkas(Ergenka[] ergenkas);
-
-}
